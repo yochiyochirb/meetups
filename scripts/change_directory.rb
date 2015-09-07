081c5947d155a1c1e16eb39e50f00c7fbc49e096
Yuka Kato <y-kato@esm.co.jp>
2015-09-07 22:01:38 +0900

    messed with dots

--- scripts/change_directory.rb
+++ scripts/change_directory.rb
@@ -3,10 +3,13 @@ Dir.chdir("/Users/yucao24hours/work/yochiyochi/meetups/reports")
 puts Dir.pwd
 
 Dir.entries(Dir.pwd).each do |entry|
-  year = entry[0..3]
-  month = entry[4..5]
-  day = entry[6..7]
-  path = year +"/"+ month +"/"+ day
+	puts entry
+	if entry != "." || entry != ".."
+		year = entry[0..3]
+		month = entry[4..5]
+		day = entry[6..7]
+		path = year + "/" + month + "/" + day
+	end
 puts path
   #Dir.mkdir(year)
 end
