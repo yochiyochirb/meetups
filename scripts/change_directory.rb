cddc7b882a4411649abb2f2dd488e7105f29fc96
Yuka Kato <y-kato@esm.co.jp>
2015-09-07 21:28:46 +0900

    techcamp team

--- scripts/change_directory.rb
+++ scripts/change_directory.rb
@@ -2,3 +2,10 @@ puts Dir.pwd
 Dir.chdir("/Users/yucao24hours/work/yochiyochi/meetups/reports")
 puts Dir.pwd
 
+Dir.entries(Dir.pwd).each do |entry|
+	year = entry.str[0...4]
+	month = entry.str[5.6]
+	day = entry.str[7.8]
+	puts year
+end
+
