28647336d9c923bc45ffa07c09792cae4e9c9996
yuzoiwasaki <a0556017@sophia.jp>
2015-09-19 15:42:43 +0900

    make yyyy/mm/dd directories

--- scripts/change_directory.rb
+++ scripts/change_directory.rb
@@ -1,4 +1,12 @@
+require 'fileutils'
 # TODO attendees ディレクトリがすでに存在する場合は以下の処理を飛ばしたい
 
 Dir.chdir('reports')
 
+dirs = Dir.glob('*')
+dirs.each do |dir|
+  year = dir[0..3]
+  month = dir[4..5]
+  day = dir[6..7]
+  FileUtils.mkdir_p("../attendees/#{year}/#{month}/#{day}/")
+end
