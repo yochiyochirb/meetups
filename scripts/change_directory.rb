629b954b88adcc91fb8731c2a57c81c0027b24ea
Yuka Kato <y-kato@esm.co.jp>
2015-09-19 15:00:16 +0900

    Delete old codes

--- scripts/change_directory.rb
+++ scripts/change_directory.rb
@@ -1,15 +1,1 @@
-puts Dir.pwd
-Dir.chdir("./reports")
-puts Dir.pwd
 
-Dir.entries(Dir.pwd).each do |entry|
-	puts "entry:#{entry}"
-	if entry != "." || entry != ".."
-		p year = entry[0..3]
-		p month = entry[4..5]
-		p day = entry[6..7]
-		p path = year + "/" + month + "/" + day
-	end
-puts path
-  #Dir.mkdir(year)
-end
