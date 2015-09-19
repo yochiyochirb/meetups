c5a6fcdda5b01569842516c9216d202d1afe3aa5
Yuka Kato <y-kato@esm.co.jp>
2015-09-19 12:07:59 +0900

    Revert "作ってみました。パスは随時変えて頂ければと。windowsのパスなので気持ち悪いことになってます。。"
    This reverts commit 2661064df31a5ee6584afbdf567528d81707504e.

--- scripts/change_directory.rb
+++ scripts/change_directory.rb
@@ -1,22 +1,15 @@
-require 'fileutils'
+puts Dir.pwd
+Dir.chdir("/Users/yucao24hours/work/yochiyochi/meetups/reports")
+puts Dir.pwd
 
-# original取得
-originals = Dir.entries("C:/data/yochiyochi.rb/meetups-master/reports")
-originals.shift(6)
-
-originals.each do |original|
-  # パス生成
-  year = original[0..3]
-  month = original[4..5]
-  day = original[6..7]
-  path_dst = "C:/data/yochiyochi.rb/new/" + year + "/" + month
-
-  # ディレクトリ生成
-  FileUtils.mkdir_p(path_dst)
-
-  # データコピー
-  path_src = "C:/data/yochiyochi.rb/meetups-master/reports/" + original
-  path_dst = "C:/data/yochiyochi.rb/new/" + year + "/" + month + "/" + day
-  FileUtils.cp_r(path_src, path_dst)
-
+Dir.entries(Dir.pwd).each do |entry|
+	puts entry
+	if entry != "." || entry != ".."
+		year = entry[0..3]
+		month = entry[4..5]
+		day = entry[6..7]
+		path = year + "/" + month + "/" + day
+	end
+puts path
+  #Dir.mkdir(year)
 end
