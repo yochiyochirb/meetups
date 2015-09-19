cd616ea948b91b026a4ecc13752fcc792b733736
pupupopo <39chenas@gmail.com>
2015-09-19 12:32:23 +0900

    add debug code

--- scripts/change_directory.rb
+++ scripts/change_directory.rb
@@ -3,12 +3,12 @@ Dir.chdir("./reports")
 puts Dir.pwd
 
 Dir.entries(Dir.pwd).each do |entry|
-	puts entry
+	puts "entry:#{entry}"
 	if entry != "." || entry != ".."
-		year = entry[0..3]
-		month = entry[4..5]
-		day = entry[6..7]
-		path = year + "/" + month + "/" + day
+		p year = entry[0..3]
+		p month = entry[4..5]
+		p day = entry[6..7]
+		p path = year + "/" + month + "/" + day
 	end
 puts path
   #Dir.mkdir(year)
