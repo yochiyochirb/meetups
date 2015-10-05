f8a0223d0fedeb15f3948bc55614645649815074
irinoyasu <irinoyasu@gmail.com>
2015-10-05 21:30:01 +0900

    コメントアウト

--- scripts/change_directory.rb
+++ scripts/change_directory.rb
@@ -5,26 +5,26 @@ require 'fileutils'
 Dir.chdir('reports')
 
 
-# reportsディレクトリ内のディレクトリを全て取得
-dirs = Dir.glob('*')
-# 各ディレクトリを対象にする（dir)
-dirs.each do |dir|
-  # 既存のフォルダ名を分解する
-  year = dir[0..3]
-  month = dir[4..5]
-  day = dir[6..7]
+ # reportsディレクトリ内のディレクトリを全て取得
+   dirs = Dir.glob('*')
+# # 各ディレクトリを対象にする（dir)
+#   dirs.each do |dir|
+#   # 既存のフォルダ名を分解する
+#   year = dir[0..3]
+#   month = dir[4..5]
+#   day = dir[6..7]
 
-  # attendeesフォルダを作成し、年月日ごとのフォルダを作成
-  dest = "../attendees/#{year}/#{month}/#{day}/"
-  FileUtils.mkdir_p(dest)
+#   # attendeesフォルダを作成し、年月日ごとのフォルダを作成
+#   dest = "../attendees/#{year}/#{month}/#{day}/"
+#   FileUtils.mkdir_p(dest)
 
-  # 既存のフォルダに移動する
-  Dir.chdir(dir)
-  # フォルダにあるすべてのファイルのファイル名を配列で取得する
-  files = Dir.glob("*")
+#   # 既存のフォルダに移動する
+#   Dir.chdir(dir)
+#   # フォルダにあるすべてのファイルのファイル名を配列で取得する
+#   files = Dir.glob("*")
 
-  FileUtils.cp(files, dest)
-  # reportsのフォルダに移動する
-  Dir.chdir('..')
+#   FileUtils.cp(files, dest)
+#   # reportsのフォルダに移動する
+#   Dir.chdir('..')
 
-end
+# end
