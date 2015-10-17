f3bf440eb73319981c28674cc5348d637e71b2e5
ta1kt0me <p.wadachi@gmail.com>
2015-10-17 15:03:48 +0900

    [ci skip] wip

--- scripts/test/change_directory_test.rb
+++ scripts/test/change_directory_test.rb
@@ -1,7 +1,13 @@
 require "minitest/autorun"
+puts Dir.pwd
+require_relative "change_directory"
 
 class TestChangeDir < Minitest::Test
   def setup
-    @change_dir = ChangeDir.new
+    @change_dir = ::ChangeDir.new
+  end
+
+  def test_execute
+    true
   end
 end
