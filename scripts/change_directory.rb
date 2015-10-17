99b5120f128fa4dbdb08e189c2e9626626b86d72
yuzoiwasaki <a0556017@sophia.jp>
2015-10-17 15:27:33 +0900

    make class

--- scripts/change_directory.rb
+++ scripts/change_directory.rb
@@ -1,33 +1,35 @@
 require 'fileutils'
 # TODO attendees ディレクトリがすでに存在する場合は以下の処理を飛ばしたい
 
-# reportsディレクトリに移動
-Dir.chdir('reports')
+class ChangeDirectory
+  # reportsディレクトリに移動
+  Dir.chdir('reports')
 
-# reportsディレクトリ内のディレクトリを全て取得
-dirs = Dir.glob('*')
+  # reportsディレクトリ内のディレクトリを全て取得
+  dirs = Dir.glob('*')
 
-# 各ディレクトリを対象にする（dir)
-dirs.each do |dir|
-  # 既存のフォルダ名を分解する
-  year = dir[0..3]
-  month = dir[4..5]
-  day = dir[6..7]
+  # 各ディレクトリを対象にする（dir)
+  dirs.each do |dir|
+    # 既存のフォルダ名を分解する
+    year = dir[0..3]
+    month = dir[4..5]
+    day = dir[6..7]
 
-  # attendeesフォルダを作成し、年月日ごとのフォルダを作成
-  dest = "../attendees/#{year}/#{month}/#{day}/"
-  FileUtils.mkdir_p(dest)
+    # attendeesフォルダを作成し、年月日ごとのフォルダを作成
+    dest = "../attendees/#{year}/#{month}/#{day}/"
+    FileUtils.mkdir_p(dest)
 
-  # 既存のフォルダに移動する
-  Dir.chdir(dir)
+    # 既存のフォルダに移動する
+    Dir.chdir(dir)
 
-  # フォルダにあるすべてのファイルのファイル名を配列で取得する
-  files = Dir.glob("*")
+    # フォルダにあるすべてのファイルのファイル名を配列で取得する
+    files = Dir.glob("*")
 
-  dest2 = '../' + dest
-  puts dest2
-  FileUtils.cp(files, dest2)
+    dest2 = '../' + dest
+    puts dest2
+    FileUtils.cp(files, dest2)
 
-  # reportsのフォルダに移動する
-  Dir.chdir('..')
+    # reportsのフォルダに移動する
+    Dir.chdir('..')
+  end
 end
