f652a967193ea424533c54f819b9b6c3152a3a61
浦瀬信介 <urasin@urasinpro.local>
2015-11-09 21:31:24 +0900

    use variable

--- scripts/scraping/scraping.rb
+++ scripts/scraping/scraping.rb
@@ -1,6 +1,7 @@
 require 'mechanize'
 require 'nokogiri'
 
+url = 'https://ja.wikipedia.org/wiki'
 agent = Mechanize.new
-page = agent.get('https://ja.wikipedia.org/wiki')
+page = agent.get(url)
 p page
