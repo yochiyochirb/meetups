cc950cf44335d82b316d805e8b4e109f47e607f1
upinetree <t.matsumoto@everyleaf.com>
2015-11-16 21:44:27 +0900

    try to use binding.pry

--- scripts/scraping/scraping.rb
+++ scripts/scraping/scraping.rb
@@ -1,9 +1,13 @@
 require 'mechanize'
 require 'nokogiri'
+require 'pry'
 
 url = 'https://www.google.com'
 agent = Mechanize.new
 page = agent.get(url)
+
+binding.pry
+
 p page.content
 # page.form_with(id: "tsf") do |search|
 #   p search
