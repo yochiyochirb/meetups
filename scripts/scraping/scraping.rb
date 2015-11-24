1c16d52613b9d39384603ed0c6ab0080223e4abd
Sek1guch1 <ryuji@ryohiroto.com>
2015-11-25 01:08:31 +0900

    add comment to scraping.rb

--- scripts/scraping/scraping.rb
+++ scripts/scraping/scraping.rb
@@ -8,7 +8,15 @@ page = agent.get(url)
 
 binding.pry
 
+# よちよち.rb を googleの検索窓へ入力
 p page.content
+
+# 検索窓へよちよち.rbと入力する
 # page.form_with(id: "tsf") do |search|
 #   p search
 # end
+
+# 検索ボタンを押す
+
+
+# 検索結果を表示する
