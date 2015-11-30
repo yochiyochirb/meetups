cd27b427b64483060d931d34330e3837001da0b2
ta1kt0me <p.wadachi@gmail.com>
2015-11-30 22:06:39 +0900

    Execute to search よちよち.rb

--- scripts/scraping/scraping.rb
+++ scripts/scraping/scraping.rb
@@ -6,15 +6,20 @@ url = 'https://www.google.com'
 agent = Mechanize.new
 page = agent.get(url)
 
-binding.pry
+# binding.pry
+# within $('form input[type=text]')
 
 # よちよち.rb を googleの検索窓へ入力
-p page.content
+# p page.content
 
 # 検索窓へよちよち.rbと入力する
 # page.form_with(id: "tsf") do |search|
 #   p search
 # end
+form = page.forms.first
+form.field_with(name: 'q').value = 'よちよち.rb'
+a = form.submit
+binding.pry
 
 # 検索ボタンを押す
 
