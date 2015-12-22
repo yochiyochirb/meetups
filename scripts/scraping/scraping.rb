5358a6ed656692b8ff16b1cc48b413865a90a109
堀江一成 <ihorie@horieissei-no-MacBook-Pro.local>
2015-12-22 21:08:00 +0900

    Costmetics

--- scripts/scraping/scraping.rb
+++ scripts/scraping/scraping.rb
@@ -5,13 +5,13 @@ url = 'https://www.google.com'
 agent = Mechanize.new
 page = agent.get(url)
 
-
 form = page.forms.first
+
 # 検索窓へよちよち.rbと入力する
 form.field_with(name: 'q').value = 'よちよち.rb'
+
 # 検索ボタンを押す
 search_results = form.submit
-
 
 # 検索結果の1件目のリンク先に移動する
 search_results.links.each do |link|
